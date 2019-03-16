bf3a30c357bd77363283ae744bfbca2f40eb3622
deusmemes <maxin29999@gmail.com>
2019-03-16 14:04:02 +0400

    commit0

---
+++ AbstractTravelAgencyServiceImplementDataBase/AbstractDbScope.cs
@@ -0,0 +1,27 @@
+﻿using AbstractTravelAgencyModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AbstractTravelAgencyServiceImplementDataBase
+{
+    class AbstractDbScope : DbContext
+    {
+        public AbstractDbScope() : base("AbstractDatabase")
+        {
+            Configuration.ProxyCreationEnabled = false;
+            Configuration.LazyLoadingEnabled = false;
+            var ensureDLLIsCopied = System.Data.Entity.SqlServer.SqlProviderServices.Instance;
+        }
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Condition> Conditions { get; set; }
+        public virtual DbSet<Booking> Bookings { get; set; }
+        public virtual DbSet<Voucher> Vouchers { get; set; }
+        public virtual DbSet<VoucherCondition> VoucherConditions { get; set; }
+        public virtual DbSet<City> Cities { get; set; }
+        public virtual DbSet<CityCondition> CityConditions { get; set; }
+    }
+}
